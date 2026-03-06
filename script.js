d39fef43dfa49da9e5edaa10653b2a384e09206a
Beatrice <brkovalik@gmail.com>
2026-03-06 13:28:13 -0500

    finsiehd needs the css

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function discribeRoom(room) {
     } else if (room === "room1") {
         console.log("You see a room wiht peeling paint and a broom")
     } else if (room === "room2") {
-        console.log("you see a room with the shtters open")
+        console.log("you see a room with the shutters open")
     } else if (room === "room3") {
         console.log("you see a sunroom with a veiw of the grass outside")
     }
@@ -18,4 +18,37 @@ const exampleNode = {
     choices: ["go to room1","go to room2","go to room3"]
 }
 
-const storyNodes = ["outside","hallway","room1","room2","room3"]
+
+let outside = {
+    image: "images/outside_house.jpg", 
+    text:"you see a abonded house in overgrown bushes", 
+    choices:["go inside"]
+}
+
+let room1 = {
+    image: "images/room1.jpg", 
+    text:"You see a room wiht peeling paint and a broom", 
+    choices:["go back to the hallway", "look outside the window", "look around the room"]
+}
+
+let room2 = {
+    image: "images/room2.jpg", 
+    text:"you see a room with the shutters open", 
+    choices:["go back to the hallway", "go outside", "look around the room"]
+}
+
+let room3 = {
+    image: "images/room3.jpg", 
+    text:"you see a sunroom with a veiw of the grass outside", 
+    choices:["go back to the hallway", "look around the room"]
+}
+
+const storyNodes = [outside, room1, room2, room3]
+
+function displayNode(i) {
+    let node = storyNodes[i];
+    document.getElementById("img").src = node.image;
+    document.getElementById("cap").textContent = node.text;
+}
+
+displayNode(2)
